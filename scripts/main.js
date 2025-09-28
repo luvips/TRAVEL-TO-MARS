4c306ef313f9311545d5f39963a56c19579ef0e5
luvips <luviaamagaali07@gmail.com>
2025-09-28 05:07:49 +0000

    Añadir llamado de funciones en el main js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,17 @@
-
 import { initStarfield } from './stars.js';
 import { getWeatherData } from './api.js';
+import { displayWeatherData } from './ui.js';
+
 
 document.addEventListener('DOMContentLoaded', async () => {
-    
+
     initStarfield();
 
+ 
     const weather = await getWeatherData();
-   
+
+    
+    displayWeatherData(weather);
+
+    
 });
